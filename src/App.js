6e140d3a42ed35f018f4b43974646292662c4c45
Vinu Cyril <vinucyril@gmail.com>
2023-10-14 00:14:19 +0530

    router set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App font-inter">
-      <Header />
+      <Routes>
+        <Route path="/" element={<SignUp />}></Route>
+      </Routes>
     </div>
   );
 }
