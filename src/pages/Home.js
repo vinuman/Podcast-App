c36c0be4db6616dc8d5c1f90b32adfc877383167
Vinu Cyril <vinucyril@gmail.com>
2023-12-03 06:49:08 +0530

    home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,47 @@ import React from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/microphone_8333936.png";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <>
       <Header />
-      <div className=" min-h-screen px-16 py-32 flex justify-center">
+      <div className=" px-16 py-16 flex justify-center items-center max-w-[1700px] mx-auto">
         {/*  div left */}
-        <div className=" w-[50%]">
-          <h1 className="text-white text-[64px]">PodSpark</h1>
-          <p className=" text-[15px] w-[70%] pb-4 text-grey">
+        <div className=" w-[100%] h-[100vh]  flex flex-col justify-start items-center pt-32">
+          <div className="flex gap-2 items-center">
+            <motion.h1
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className="text-white text-[64px] underline"
+            >
+              PodSpark
+            </motion.h1>
+            <motion.img
+              initial={{ x: 20 }}
+              animate={{ x: 0 }}
+              transition={{
+                type: "smooth",
+                repeatType: "mirror",
+                duration: 2,
+                repeat: Infinity,
+              }}
+              className="w-[50px] h-[50px]"
+              src={logo}
+              alt="logo"
+            ></motion.img>
+          </div>
+
+          <motion.p
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            className=" text-[15px] w-[70%] pb-4 text-grey text-center"
+          >
             PodSpark, where creativity ignites and voices come alive. Unleash
             your passion for storytelling on this dynamic podcast platform.
             Whether you're a seasoned creator or a budding podcaster, PodSpark
@@ -22,9 +52,12 @@ const Home = () => {
             landscape of audio content. Join the PodSpark community and
             experience the power of sparking connections through the art of
             podcasting.
-          </p>
+          </motion.p>
           <div className="flex gap-4">
-            <div
+            <motion.div
+              initial={{ opacity: 0, x: 0 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
               onClick={() => navigate("/signup")}
               className="w-[240px] h-[52px]  rounded-lg flex items-center justify-center gap-2 cursor-pointer group bg-sky-800 mb-8 group transition-all duration-300"
             >
@@ -32,7 +65,7 @@ const Home = () => {
                 className=" text-white  font-bold group-hover:text-black"
                 text="Sign Up/ Sign In"
               />
-            </div>
+            </motion.div>
           </div>
         </div>
         {/* LEFT DIV ENDS */}
